c116e3548628324c4389729426204e4fa8ca711d
kaskimaki <artturi.kaskimaki@aalto.fi>
2025-03-03 20:47:37 +0200

    TailwindCS to NextJS config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,8 +3,8 @@ const config = {
     content: [
       "./src/**/*.{js,ts,jsx,tsx}",  
       "./app/**/*.{js,ts,jsx,tsx}",
-      "./components/**/*.{js,ts,jsx,tsx}",
-      "./pages/**/*.{js,ts,jsx,tsx}"
+      "./Components/**/*.{js,ts,jsx,tsx}",
+      "./pages/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
       extend: {},
