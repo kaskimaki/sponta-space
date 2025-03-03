b48aaf6140e72718bd4342a1bb3ecc305ed1491d
kaskimaki <artturi.kaskimaki@aalto.fi>
2025-03-03 21:14:12 +0200

    Fix Tailwind prebuild issue

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,15 @@
-import type { Config } from 'tailwindcss'
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+const config = {
   content: [
     "./src/**/*.{js,ts,jsx,tsx}",
     "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}", // ❌ Fix "Components" -> "components" (folder name case)
+    "./components/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
 
 export default config;
