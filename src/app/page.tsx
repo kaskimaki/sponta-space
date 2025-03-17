6807550472bb17b3245212d117433ae39368a1bf
kaskimaki <artturi.kaskimaki@aalto.fi>
2025-03-17 16:57:24 +0200

    Changed logo paramters

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import { Button } from "./components/ui/button";
 import { Footer } from "./components/ui/footer";
 import { Header } from "./components/ui/header";
 import Image from "next/image";
+import logo from "../public/logo.png"
+
 // ✅ Explicitly enable dynamic rendering
 export const dynamic = "force-dynamic";
 
@@ -18,7 +20,7 @@ export default function LandingPage() {
       }}
     >
       <Header>
-        <Image src="/logo.png" alt="Sponta Logo" width={500} height={200} />
+        <Image src={logo} alt="Sponta Logo" width={500} height={200} />
       </Header>
       <div
         style={{
