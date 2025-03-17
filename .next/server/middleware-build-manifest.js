e0edd3f4bc905843c74fd936de0ea09853c09cde
kaskimaki <artturi.kaskimaki@aalto.fi>
2025-03-17 17:24:26 +0200

    Changed logo paramters4

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -1,1 +1,19 @@
-globalThis.__BUILD_MANIFEST={polyfillFiles:["static/chunks/polyfills-42372ed130431b0a.js"],devFiles:[],ampDevFiles:[],lowPriorityFiles:[],rootMainFiles:["static/chunks/webpack-f029a09104d09cbc.js","static/chunks/4bd1b696-49be05dc45317ca5.js","static/chunks/684-ef14dbc17fbb0470.js","static/chunks/main-app-5e528383ddf62b62.js"],rootMainFilesTree:{},pages:{"/_app":["static/chunks/webpack-f029a09104d09cbc.js","static/chunks/framework-859199dea06580b0.js","static/chunks/main-48abd9ad937b12e1.js","static/chunks/pages/_app-da15c11dea942c36.js"],"/_error":["static/chunks/webpack-f029a09104d09cbc.js","static/chunks/framework-859199dea06580b0.js","static/chunks/main-48abd9ad937b12e1.js","static/chunks/pages/_error-cc3f077a18ea1793.js"]},ampFirstPages:[]},globalThis.__BUILD_MANIFEST.lowPriorityFiles=["/static/"+process.env.__NEXT_BUILD_ID+"/_buildManifest.js",,"/static/"+process.env.__NEXT_BUILD_ID+"/_ssgManifest.js"];
+globalThis.__BUILD_MANIFEST = {
+  "polyfillFiles": [
+    "static/chunks/polyfills.js"
+  ],
+  "devFiles": [],
+  "ampDevFiles": [],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [],
+  "rootMainFilesTree": {},
+  "pages": {
+    "/_app": []
+  },
+  "ampFirstPages": []
+};
+globalThis.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+,"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
+
+];
